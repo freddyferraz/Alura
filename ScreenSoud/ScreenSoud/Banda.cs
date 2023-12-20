5f4fb7cd5bafaf8a64162c58ced9062a6b10ab7f
freddyferraz <68133605+freddyferraz@users.noreply.github.com>
2023-12-20 14:49:17 -0300

    Add files via upload

---
+++ ScreenSoud/ScreenSoud/Banda.cs
@@ -0,0 +1,25 @@
+﻿class Banda
+{
+    public string Nome { get;}
+    private List<Album> albums = new List<Album>();
+
+    public Banda(string nome)
+    {
+        Nome = nome;
+    }
+    public void AdicionarAlbum(Album album)
+    {
+        albums.Add(album);
+    }
+
+    public void ExibirDiscografia()
+    {
+        Console.WriteLine($"Discografia da Banda {Nome}");
+        foreach(Album album in albums)
+        {
+            Console.WriteLine($"Album: {album.Nome}");
+
+            album.ExibirMusicasAlbum();
+        }
+    }
+}
---
+++ ScreenSoud/ScreenSoud/Musica.cs
@@ -0,0 +1,46 @@
+﻿class Musica
+{
+  
+
+    public string Nome {  get; }
+    public Banda Artista { get; }
+
+    public Musica(Banda artista, string nome)
+    {
+        Artista = artista;
+        Nome = nome;
+    }
+
+    public int Duracao { get; set; }
+    public bool Disponivel { get; set; }
+    public Genero Genero { get; set; }
+
+    public string DescricaoResumida {
+        get
+        {
+            return $"A música {Nome} pertence a banda {Artista}";
+        } 
+    }
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome:{Nome}");
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Duração: {Duracao}");
+        if ( Disponivel ==  true)
+        {
+            Console.WriteLine("Disponível no plano");
+        }
+        else
+        {
+            Console.WriteLine("Disponível no plano Plus");
+        }
+    }
+
+    public void ExibirResumo()
+    {
+        Console.WriteLine($"A música {Nome} pertence ao artista {Artista}");
+    }
+
+  
+}
