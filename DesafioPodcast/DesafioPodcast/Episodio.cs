6a1f4710ec577d935aa8e432b367368fd8390312
freddyferraz <68133605+freddyferraz@users.noreply.github.com>
2023-12-20 14:47:17 -0300

    Add files via upload

---
+++ DesafioPodcast/DesafioPodcast/Episodio.cs
@@ -0,0 +1,20 @@
+﻿class Episodio
+{
+    private List<string> convidados = new List<string>();
+    public Episodio(int ordem, string titulo, int duracao)
+    {
+        Titulo = titulo;
+        Ordem = ordem;
+        Duracao = duracao;
+    }
+
+    public string Titulo {get;}
+    public int Ordem {get;}
+    public int Duracao {get;}
+    public string Reusmo => $"{Ordem}. {Titulo} ({Duracao} min) - {string.Join(",",convidados)}";
+
+    public void AdicionarConvidados(string convidado)
+    {
+        convidados.Add(convidado);
+    }
+}
