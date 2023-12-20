30dccee2025491a12583609a3b21979838cfa688
freddyferraz <68133605+freddyferraz@users.noreply.github.com>
2023-12-20 15:01:04 -0300

    Add files via upload

---
+++ DesafioPodcast/DesafioPodcast/Podecast.cs
@@ -0,0 +1,28 @@
+﻿class Podcast
+{
+    private List<Episodio> episodioList = new();    
+    public Podcast(string nome, string host)
+    {
+        Nome = nome;
+        Host = host;
+    }
+
+    public string Nome {get;}
+    public string Host {get;}
+    public int TotalEpisodios => episodioList.Count;
+
+    public void AdicionarEpisodio(Episodio episodio)
+    {
+        episodioList.Add(episodio);
+    }
+
+    public void ExibirDetalhes()
+    {
+        Console.WriteLine($"Podcast: {Nome}. Apresentado por {Host}\n");
+        foreach(Episodio episodio in episodioList.OrderBy(e => e.Ordem))
+        {
+            Console.WriteLine(episodio.Reusmo);
+        }
+        Console.WriteLine($"Este Podcast possui {TotalEpisodios} episodios");
+    }
+}
